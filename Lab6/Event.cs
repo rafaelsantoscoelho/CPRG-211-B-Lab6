cc5cc3ef714b54f1093354bcaa4aa5c00a2a6381
Rafael Santos Coelho <141067320+rafaelsantoscoelho@users.noreply.github.com>
2023-11-16 10:45:48 -0700

    Add files via upload

---
+++ Lab6/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+	internal class Event
+	{
+		public int EventNumber {  get; set; }
+		public string Location { get; set; }
+
+		public Event(int eventNumber, string location)
+		{
+			EventNumber = eventNumber;
+			Location = location;
+		}	
+	}
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+
+namespace Lab6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Event e = new Event(1, "Calgary");
+
+			// Console.WriteLine(e.EventNumber);
+			// Console.WriteLine(e.Location);
+
+			string JSONfileName = "events.json";
+			string encoded = JsonSerializer.Serialize(e);
+			File.WriteAllText(JSONfileName, encoded);
+
+			string fileContent = File.ReadAllText(JSONfileName);
+			Event decoded = JsonSerializer.Deserialize<Event>(fileContent);
+
+			Console.WriteLine(decoded.EventNumber);
+			Console.WriteLine(decoded.Location);
+
+			Console.WriteLine("Tech Competition");
+
+			string HackathonFileName = "hackathon.txt";
+			ReadFromFile(HackathonFileName);
+		}
+
+		static void ReadFromFile(string fileName) 
+		{
+			Console.WriteLine("In Word: Hackathon");
+			using(StreamWriter sw = new StreamWriter(fileName))
+			{
+				sw.Write("Hackathon");
+			}
+
+			using (StreamReader sr = new StreamReader(fileName))
+			{
+				sr.BaseStream.Seek(0, SeekOrigin.Begin);
+				Console.WriteLine("The First Character is: \"{0}\"", (char)sr.Read());
+
+				sr.DiscardBufferedData();
+				sr.BaseStream.Seek(4, SeekOrigin.Begin);
+				Console.WriteLine("The Middle Character is: \"{0}\"", (char)sr.Read());
+
+				sr.DiscardBufferedData();
+				sr.BaseStream.Seek(8, SeekOrigin.Begin);
+				Console.WriteLine("The Last Character is: \"{0}\"", (char)sr.Read());
+			}
+		}
+	}
+}
